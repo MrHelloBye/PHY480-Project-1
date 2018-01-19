dbcd25bd6e22ab52f28c126035e3ae357dafd2d3
MrHelloBye <MrHelloBye@users.noreply.github.com>
2018-01-19 17:49:03 -0500

    Added code for initializing matrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 
-double* init_matrix(unsigned int);
+double** init_matrix(unsigned int);
+void populate_matrix(double**);
 
 
 int main()
@@ -9,7 +10,17 @@ int main()
     return 0;
 }
 
-double* init_matrix(unsigned int size)
+double** init_matrix(unsigned int size)
 {
+    double** array = new double*[size];
+    for (unsigned int i = 0; i<size; i++)
+    {
+        array[i] = new double[size];
+        
+        // Initialize elements to zero
+        for (unsigned int j = 0; j<size; j++)
+            array[i][j] = 0;
+    }
     
+    return array;
 }
